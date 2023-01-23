45b3471a0cb06e4203ec4cde78cbf832b593f8fa
meatsby <meatsby@gmail.com>
2023-01-23 18:01:59 +0800

    test: SingletonTest

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -21,4 +21,13 @@ public class SingletonTest {
 
         assertThat(memberService1).isNotSameAs(memberService2);
     }
+
+    @Test
+    @DisplayName("싱글톤 객체 사용")
+    void singletonServiceTest() {
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        assertThat(singletonService1).isSameAs(singletonService2);
+    }
 }
